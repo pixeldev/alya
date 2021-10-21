fdc907de19ab66718f6e8646c79652d1a11cb406
pixeldev <amiranda8695@gmail.com>
2021-10-21 15:23:44 -0500

    just test.

--- bukkit/src/main/java/me/pixeldev/alya/bukkit/auto/BukkitAutoListenerAnnotationProcessor.java
+++ bukkit/src/main/java/me/pixeldev/alya/bukkit/auto/BukkitAutoListenerAnnotationProcessor.java
@@ -30,7 +30,6 @@ public class BukkitAutoListenerAnnotationProcessor
 			out.println("  @Override");
 			out.println("  public void load() {");
 			out.println("    org.bukkit.plugin.PluginManager pluginManager = org.bukkit.Bukkit.getPluginManager();");
-			out.println("    pluginManager.registerEvents(new team.unnamed.gui.core.GUIListeners(), plugin);");
 
 			elements.forEach(listener -> out.println(
 					"    pluginManager.registerEvents(" + listener.getSimpleName().toString().toLowerCase() + ", plugin);"
